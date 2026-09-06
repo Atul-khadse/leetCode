d48b455363ef98f17674b687e02660ba804c1205
Atul Khadse <158227880+Atul-khadse@users.noreply.github.com>
2026-09-06 20:46:45 +0530

    Time: 0 ms (100%), Space: 43.4 MB (42.83%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+
+     for (int i = digits.length - 1; i >= 0; i--) {
+            
+            
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            
+           
+            digits[i] = 0;
+        }
+
+
+        int[] newNo = new int[digits.length+1];
+
+        newNo[0] = 1;
+
+        return newNo;
+        
+
+        
+    }
+}
